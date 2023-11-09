4a43d29d9c387f977cfbbbf873c421e3c04e3c74
aldo-castellano <aldofcastellanop@gmail.com>
2023-11-09 22:51:40 +0100

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "@/styles/page.module.css";
+import Main from "@/components/main/main";
+
+export default function Home() {
+  return (
+    <main>
+      <Main />
+    </main>
+  );
+}
