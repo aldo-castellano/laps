87ff1b3f6ba20f31e72da48380683cd3613d3aab
aldo-castellano <92597205+aldo-castellano@users.noreply.github.com>
2023-11-18 19:53:48 +0100

    add reviews section

---
+++ src/components/main/reviews.tsx
@@ -0,0 +1,64 @@
+import styles from "@/styles/reviews.module.css";
+import toolStyles from "@/styles/general/tools.module.css";
+
+export default function Reviews() {
+  const cards = [
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+        quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+        culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+        veritatis nesciunt quam laborum molestias.`,
+    },
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+          culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+          veritatis nesciunt quam laborum molestias.`,
+    },
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+          culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+          veritatis nesciunt quam laborum molestias.`,
+    },
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+          culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+          veritatis nesciunt quam laborum molestias.`,
+    },
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+          culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+          veritatis nesciunt quam laborum molestias.`,
+    },
+    {
+      name: "Aldo",
+      reviews: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta,
+          quibusdam minus. Quisquam consequuntur nemo repudiandae soluta iste
+          culpa alias quod quos eius. Quibusdam pariatur voluptatibus,
+          veritatis nesciunt quam laborum molestias.`,
+    },
+  ];
+  return (
+    <div className={toolStyles.container}>
+      <h2>Reseñas</h2>
+      <div className={styles.container_card}>
+        {cards.map((card: { name: string; reviews: string }) => {
+          return (
+            <div className={styles.card}>
+              <p className={styles.name}>{card.name}</p>
+              <article className={styles.review}>{card.reviews}</article>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
