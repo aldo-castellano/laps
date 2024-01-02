e510fdbb3e8b16c64b353800903ce79df58a2c29
aldo-castellano <aldofcastellanop@gmail.com>
2024-01-02 13:36:21 +0100

    make us

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import Form from "./form/form";
+import Feature from "./feature/feature";
 
 export default function Home() {
   return (
     <main>
+      <Feature />
       <Form />
     </main>
   );
